1006fa2664425e219445c2528dba40944f9408c7
elpabloultron <elpabloultron@users.noreply.github.com>
2026-08-30 13:16:32 -0400

    fix: linter en dga_telemetria.py

--- dga_telemetria.py
+++ dga_telemetria.py
@@ -1,5 +1,5 @@
 import time
-import logging
+
 
 def extraer_telemetria_bnaconsultas(estaciones):
     """
@@ -8,12 +8,12 @@ def extraer_telemetria_bnaconsultas(estaciones):
     Se requiere API Key oficial (MEE o HIDROlínea) de la DGA para continuar.
     """
     telemetria_global = {}
-        
+
     for est in estaciones:
         st_id = est["id"]
         tele = est.copy()
         tele["timestamp_actualizacion"] = int(time.time())
         # No se inyectan variables físicas para forzar el "Sin reporte"
         telemetria_global[st_id] = tele
-        
+
     return telemetria_global
